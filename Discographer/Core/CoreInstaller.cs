339ac65c5fa3087168f28f331a153a211dd19a30
Michael Dahl <micdah@gmail.com>
2017-10-29 01:27:12 +0200

    Add AutoFac

---
+++ Discographer/Core/CoreInstaller.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Microsoft.Extensions.DependencyInjection;
+using Autofac.Extensions.DependencyInjection;
+namespace Discographer.Core
+{
+    public static class CoreInstaller
+    {
+        public static ContainerBuilder InstallServiceCollection(this ContainerBuilder builder, IServiceCollection serviceCollection) 
+        {
+            builder.Populate(serviceCollection);
+            return builder;
+        }
+
+        public static ContainerBuilder InstallCore(this ContainerBuilder builder)
+        {
+            return builder;
+        }
+    }
+}
--- Discographer/Startup.cs
+++ Discographer/Startup.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Discographer.Core;
 
 namespace Discographer
 {
@@ -12,12 +16,21 @@ namespace Discographer
             Configuration = configuration;
         }
 
+        public IContainer Container { get; private set; }
+
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // Setup AutoFac
+            Container = new ContainerBuilder()
+                .InstallServiceCollection(services)
+                .InstallCore()
+                .Build();
+
+            return new AutofacServiceProvider(Container);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
